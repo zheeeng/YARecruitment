898eff46ad564a7f9f904b95f414de967a5dc6bd
Zheeeng <air.island.e@gmail.com>
2016-10-12 22:04:47 +0800

    Add the layout for the app framework

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import 'styles/app.scss'
+
+import React from 'react'
+
+let App = React.createClass({
+  render () {
+    return (
+      <div className='app'>
+        <div className='app_leftsider' />
+        <div className='app_rightsider' />
+      </div>
+    )
+  }
+})
+
+export default App
--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
 import 'styles/base.scss'
+import App from 'components/App'
+
+import React from 'react'
+import { render } from 'react-dom'
+
+render(<App />, document.getElementById('app'))
