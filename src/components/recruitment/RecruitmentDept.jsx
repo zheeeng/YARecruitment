93fdea61712b593ec6e0a5a2371710206d2fcb90
Zheeeng <air.island.e@gmail.com>
2016-10-13 21:22:45 +0800

    Add handleCheckboxChange method

--- src/components/recruitment/RecruitmentDept.jsx
+++ src/components/recruitment/RecruitmentDept.jsx
@@ -3,13 +3,19 @@ import React from 'react'
 import RecruitmentPosition from 'components/recruitment/RecruitmentPosition'
 
 let RecruitmentDept = React.createClass({
+  getInitialState () {
+    return {check: false}
+  },
+  handleCheckboxChange (e) {
+    this.setState({check: !this.state.check})
+  },
   render () {
     return (
       <div className='recruitment-dept'>
         <div className='recruitment-entry wings_box wings_box__right3r'>
           <label className='recruitment-entry_checkbar highlight wings_item wings_item__main'>
-            <input type='checkbox' className='input-hide' />
-            <i className='recruitment-entry_checkbox fa fa-square-o' />
+            <input type='checkbox' className='input-hide' checked={this.state.check} onChange={this.handleCheckboxChange}/>
+            <i className={'recruitment-entry_checkbox fa fa-' + (this.state.check ? 'check-' : '') + 'square-o'} />
             <span className='recruitment-entry_name highlight'>工程研发部</span>
             <i className='recruitment-entry_expand highlight fa fa-angle-down' />
           </label>
